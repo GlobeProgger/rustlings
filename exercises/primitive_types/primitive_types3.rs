539b53ede6bc0c0e75be1af4521ad2d9a46f2c65
mojawo <mojawog@gmail.com>
2022-07-25 13:59:55 +0000

    completed 'primitive_types'

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -2,14 +2,14 @@
 // Create an array with at least 100 elements in it where the ??? is.
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
+use std::io;
 
 fn main() {
-    let a = ???
+    let a:[&str;1001]  = ["i32"; 1001];
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
-    }
+    } 
 }
