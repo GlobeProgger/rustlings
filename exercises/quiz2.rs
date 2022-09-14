1f5007274def5838a29a6ed5f7b415786ee8fba5
mojawo <mojawo@gmail.com>
2022-09-14 15:13:01 +0200

    Completed 'quiz3'

--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -18,8 +18,6 @@
 // - The output element is going to be a Vector of strings.
 // Execute `rustlings hint quiz2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub enum Command {
     Uppercase,
     Trim,
@@ -29,12 +27,29 @@ pub enum Command {
 mod my_module {
     use super::Command;
 
-    // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
-        // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+    // Complete the function signature!
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
+        // Complete the output declaration!
+        let mut output: Vec<String> = vec!["".to_string(); input.len()];
+        let mut index = 0;
         for (string, command) in input.iter() {
-            // TODO: Complete the function body. You can do it!
+            //  Complete the function body. You can do it!
+            let mut element = "".to_string();
+            match command {
+                Command::Uppercase => {
+                    element = string.to_uppercase();
+                }
+                Command::Trim => element = string.trim().to_string(),
+                Command::Append(amount) => {
+                    element = string.to_string();
+
+                    for _ in 0..*amount as i32 {
+                        element.push_str("bar");
+                    }
+                }
+            }
+            output.insert(index, element);
+            index += 1;
         }
         output
     }
@@ -42,9 +57,9 @@ mod my_module {
 
 #[cfg(test)]
 mod tests {
-    // TODO: What to we have to import to have `transformer` in scope?
-    use ???;
+    // What to we have to import to have `transformer` in scope?
     use super::Command;
+    use my_module::transformer;
 
     #[test]
     fn it_works() {
