acc2f32677e50604c4ef9e8d569fcee12e131c98
globegprogger <globeprogger@gmail.com>
2022-09-26 14:30:16 +0200

    Continued 'Errors'

--- exercises/error_handling/errors3.rs
+++ exercises/error_handling/errors3.rs
@@ -11,14 +11,23 @@ use std::num::ParseIntError;
 fn main() {
     let mut tokens = 100;
     let pretend_user_input = "8";
+    println!("Error parsing user input");
+    let cost = total_cost(pretend_user_input);
 
-    let cost = total_cost(pretend_user_input)?;
-
-    if cost > tokens {
-        println!("You can't afford that many!");
-    } else {
-        tokens -= cost;
-        println!("You now have {} tokens.", tokens);
+    match cost {
+        Ok(cost) => {
+            if cost > tokens {
+                println!("----You can't afford that many!");
+            } else {
+                tokens -= cost;
+                println!(
+                    "You spend {} tokens to buy {} items \n\
+                    You now have {} tokens left.",
+                    cost, pretend_user_input, tokens,
+                );
+            }
+        }
+        Err(_) => print!("Error parsing user input"),
     }
 }
 
