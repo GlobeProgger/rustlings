78f742d2da9e1cb42d225b6ffdc8b77e9857c43c
mojawo <mojawo@gmail.com>
2023-03-06 17:49:46 +0100

    Threads1 done

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -4,18 +4,16 @@
 
 // I AM NOT DONE
 
-use std::thread;
+use std::thread::{self, JoinHandle};
 use std::time::Duration;
 
-
 fn main() {
-
-    let mut handles = vec![];
+    let mut handles: Vec<JoinHandle<()>> = vec![];
     for i in 0..10 {
-        thread::spawn(move || {
+        handles.push(thread::spawn(move || {
             thread::sleep(Duration::from_millis(250));
             println!("thread {} is complete", i);
-        });
+        }));
     }
 
     let mut completed_threads = 0;
@@ -27,5 +25,4 @@ fn main() {
     if completed_threads != 10 {
         panic!("Oh no! All the spawned threads did not finish!");
     }
-    
 }
