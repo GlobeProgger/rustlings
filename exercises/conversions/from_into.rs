40379aff6d4d051e6169b250d3728060ff5acafa
globegprogger <globeprogger@gmail.com>
2023-03-07 23:38:57 +0100

    from_into done

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -3,6 +3,8 @@
 // You can read more about it at https://doc.rust-lang.org/std/convert/trait.From.html
 // Execute `rustlings hint from_into` or use the `hint` watch subcommand for a hint.
 
+use std::process::id;
+
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -35,10 +37,31 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            return Person::default();
+        }
+        let mut split = s.split(",");
+        let name = split.next().unwrap();
+        if name.len() == 0 {
+            return Person::default();
+        }
+        let res = split.next();
+        if res.is_none() {
+            return Person::default();
+        }
+        if split.next().is_some() {
+            return Person::default();
+        }
+        let age = res.unwrap().parse::<usize>();
+        match age {
+            Ok(age) => Person {
+                name: String::from(name),
+                age,
+            },
+            Err(_) => Person::default(),
+        }
     }
 }
 
